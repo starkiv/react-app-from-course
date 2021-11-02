6858692894d92d494af278d57b8b5dc15d665b37
Yurii Starkiv <yuriistarkiv@gmail.com>
2021-11-02 17:15:19 +0000

    The first changes

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+import "./ExpenseItem.css";
+
+function ExpenseItem() {
+  return (
+    <div className="expense-item">
+      <div>November 2 2021</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">£68.5</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
